e98e34aa623a393924f244da537c233f8e7d3877
DanRuizPeru <u202210764@upc.edu.pe>
2025-10-11 06:19:18 -0500

    chore:use navbar in transport view

--- src/domains/transport/presentation/pages/TransportView.vue
+++ src/domains/transport/presentation/pages/TransportView.vue
@@ -2,10 +2,11 @@
 import FastRecharge from "../../../../shared/ui/transport/components/fast-recharge.component.vue";
 import RecentTransactions from "../../../../shared/ui/transport/components/recent-transactions.component.vue";
 import TransportCard from "../../../../shared/ui/transport/components/transport-card.component.vue";
+import NavBar from "../../../../shared/presentation/components/nav-bar.vue";
 </script>
 
 <template>
-
+  <nav-bar></nav-bar>/
   <section class="transport-section">
     <div class="transport-card">
       <transport-card/>
